62d3fc931a18c389e94d8c5b8b268af4380660e0
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-05-29 00:50:26 +0200

    add player

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,43 @@
 #![warn(clippy::all, clippy::pedantic)]
+#![allow(clippy::cast_possible_truncation)]
 
-use bracket_lib::terminal::{main_loop, BError, BTerm, BTermBuilder, GameState, VirtualKeyCode};
+use bracket_lib::{
+    color::{BLACK, NAVY, YELLOW},
+    terminal::{main_loop, to_cp437, BError, BTerm, BTermBuilder, GameState, VirtualKeyCode},
+};
+
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
 
 struct State {
+    player: Player,
+    frame_time: f32,
     mode: GameMode,
 }
 impl State {
     fn new() -> Self {
         State {
+            player: Player::new(5, 25),
+            frame_time: 0.0,
             mode: GameMode::Menu,
         }
     }
     fn play(&mut self, ctx: &mut BTerm) {
-        self.mode = GameMode::End;
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.grav_and_move();
+        }
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press SPACE to flap");
+        if self.player.y > SCREEN_HEIGHT {
+            self.mode = GameMode::End;
+        }
     }
     fn menu(&mut self, ctx: &mut BTerm) {
         ctx.cls();
@@ -28,6 +53,8 @@ impl State {
         }
     }
     fn restart(&mut self) {
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
         self.mode = GameMode::Playing;
     }
     fn dead(&mut self, ctx: &mut BTerm) {
@@ -58,6 +85,33 @@ enum GameMode {
     Menu,
     Playing,
     End,
+}
+
+struct Player {
+    x: i32,
+    y: i32,
+    v: f32,
+}
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player { x, y, v: 0.0 }
+    }
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(self.x, self.y, YELLOW, BLACK, to_cp437('@'));
+    }
+    fn grav_and_move(&mut self) {
+        if self.v < 2.0 {
+            self.v += 0.2;
+        }
+        self.y += self.v as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+    fn flap(&mut self) {
+        self.v = -2.0;
+    }
 }
 
 fn main() -> BError {
