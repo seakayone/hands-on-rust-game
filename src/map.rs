5f5946355e26cf3d8553b5243c854516bef31592
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-06-03 14:06:12 +0200

    basic map

---
+++ src/map.rs
@@ -0,0 +1,42 @@
+#![warn(clippy::all, clippy::pedantic)]
+
+use crate::prelude::*;
+
+const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum TileType {
+    Wall,
+    Floor,
+}
+
+pub struct Map {
+    tiles: Vec<TileType>,
+}
+
+pub fn map_idx(x: i32, y: i32) -> usize {
+    ((y * SCREEN_WIDTH) + x) as usize
+}
+
+impl Map {
+    pub fn new() -> Self {
+        Self {
+            tiles: vec![TileType::Floor; NUM_TILES],
+        }
+    }
+    pub fn render(&self, ctx: &mut BTerm) {
+        for y in 0..SCREEN_HEIGHT {
+            for x in 0..SCREEN_WIDTH {
+                let idx = map_idx(x, y);
+                match self.tiles[idx] {
+                    TileType::Wall => {
+                        ctx.set(x, y, RED, BLACK, to_cp437('#'));
+                    }
+                    TileType::Floor => {
+                        ctx.set(x, y, YELLOW, BLACK, to_cp437('.'));
+                    }
+                }
+            }
+        }
+    }
+}
