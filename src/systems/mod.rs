ff705e42c91b5eb459a4932a3bb1145961d90bcd
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-06-05 13:56:05 +0200

    add collisions

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,12 +1,14 @@
 #![warn(clippy::all, clippy::pedantic)]
 use crate::prelude::*;
 
+mod collisions;
 mod entity_render;
 mod map_render;
 mod player_input;
 
 pub fn build_scheduler() -> Schedule {
     Schedule::builder()
+        .add_system(collisions::collisions_system())
         .add_system(player_input::player_input_system())
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
