6c02829520998381cbf6ef81642618bb05c9bd56
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-06-05 07:56:54 +0200

    add ecs

---
+++ src/components.rs
@@ -0,0 +1,12 @@
+#![warn(clippy::all, clippy::pedantic)]
+
+use crate::prelude::*;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Render {
+    pub color: ColorPair,
+    pub glyph: FontCharType,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Player;
