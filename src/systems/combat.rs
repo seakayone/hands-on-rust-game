64a97b4f71ab085e6c121192e8ce5ca6c9963660
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-06-07 16:58:07 +0200

    add combat

---
+++ src/systems/combat.rs
@@ -0,0 +1,27 @@
+#![warn(clippy::all, clippy::pedantic)]
+use crate::prelude::*;
+
+#[system]
+#[read_component(WantsToAttack)]
+#[write_component(Health)]
+pub fn combat(ecs: &mut SubWorld, commands: &mut CommandBuffer) {
+    let mut attackers = <(Entity, &WantsToAttack)>::query();
+    let victims: Vec<(Entity, Entity)> = attackers
+        .iter(ecs)
+        .map(|(entity, attack)| (*entity, attack.victim))
+        .collect();
+
+    victims.iter().for_each(|(attacker, victim)| {
+        if let Ok(mut health) = ecs
+            .entry_mut(*victim)
+            .unwrap()
+            .get_component_mut::<Health>()
+        {
+            health.current -= 1;
+            if health.current < 1 {
+                commands.remove(*victim);
+            }
+        }
+        commands.remove(*attacker);
+    });
+}
--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -2,6 +2,7 @@
 use crate::prelude::*;
 
 mod collisions;
+mod combat;
 mod end_turn;
 mod entity_render;
 mod hud;
@@ -24,6 +25,8 @@ pub fn build_input_scheduler() -> Schedule {
 
 pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
+        .add_system(combat::combat_system())
+        .flush()
         .add_system(movement::movement_system())
         .flush()
         .add_system(collisions::collisions_system())
@@ -38,6 +41,8 @@ pub fn build_player_scheduler() -> Schedule {
 pub fn build_monster_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(random_move::random_move_system())
+        .flush()
+        .add_system(combat::combat_system())
         .flush()
         .add_system(movement::movement_system())
         .flush()
