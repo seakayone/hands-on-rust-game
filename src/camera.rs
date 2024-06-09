31a8c1cb539067145ac677daf18b47c76b9c112f
Christian Kleinbölting <christian.kleinboelting@dasch.swiss>
2024-06-10 01:47:34 +0200

    fix camera

--- src/camera.rs
+++ src/camera.rs
@@ -16,15 +16,15 @@ impl Camera {
         Self {
             left_x: player_position.x - DISPLAY_WIDTH / 2,
             right_x: player_position.x + DISPLAY_WIDTH / 2,
-            top_y: player_position.y - DISPLAY_WIDTH / 2,
-            bottom_y: player_position.y + DISPLAY_WIDTH / 2,
+            top_y: player_position.y - DISPLAY_HEIGHT / 2,
+            bottom_y: player_position.y + DISPLAY_HEIGHT / 2,
         }
     }
 
     pub fn on_player_move(&mut self, player_position: Point) {
         self.left_x = player_position.x - DISPLAY_WIDTH / 2;
         self.right_x = player_position.x + DISPLAY_WIDTH / 2;
-        self.top_y = player_position.y - DISPLAY_WIDTH / 2;
-        self.bottom_y = player_position.y + DISPLAY_WIDTH / 2;
+        self.top_y = player_position.y - DISPLAY_HEIGHT / 2;
+        self.bottom_y = player_position.y + DISPLAY_HEIGHT / 2;
     }
 }
